fa7dd2b9f1031e1e09a7676e07545594b8a93b42
amarse <a.marquez.seco@hotmail.com>
2023-11-22 09:45:21 +0100

    Add simple Entity model

--- csv_upload_back/csv_validation/models.py
+++ csv_upload_back/csv_validation/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django import forms
+from django.core import validators
 
-# Create your models here.
+
+class Entity(models.Model):
+    entity_id = models.CharField(max_length=8)
+    data_type = models.CharField(max_length=16)
+    data_value = models.CharField(max_length=256)
